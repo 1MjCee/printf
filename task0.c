30faf7edef3e28ce090f22fd3201f932c2880f41
Tuyisenge Jordan <jordanale.112@gmail.com>
2023-11-13 22:05:44 +0200

    compiled with task0 and change test0 to .k

--- task0.c
+++ task0.c
@@ -47,3 +47,11 @@ int _printf(const char *format, ...)
 	va_end(recorded_arguments);
 	return (letter_counter);
 }
+
+int main(void)
+{
+  _printf("hello, %s\n", "Jordan");
+  _printf("sign %%\n");
+  _printf("grade: %c\n", 'A');
+  return (0);
+}
