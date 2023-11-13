f0fa8ebb0424b2de12fcf5e59c5a3f47bafdfe20
Tuyisenge Jordan <jordanale.112@gmail.com>
2023-11-13 21:19:27 +0200

    task 0 work

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
 int _putchar(char c);
 size_t _strlen(const char *str);
 
