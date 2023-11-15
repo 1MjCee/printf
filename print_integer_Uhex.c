d6006a3f0a5a945b8ebb2d5ed89f56518b46f8a0
Julius Charles <cjmatheka@gmail.com>
2023-11-15 13:34:01 -0800

    Update files to unix

--- print_integer_Uhex.c
+++ print_integer_Uhex.c
@@ -9,37 +9,39 @@
 
 int print_hexadecimal_upper(va_list args)
 {
-    int len, i;
-    unsigned int hexDigits[8];
-
-    unsigned int value = va_arg(args, unsigned int);
-
-    /* Handle the case of zero */
-    if (value == 0) {
-        _putchar('0');
-        return (1);
-    }
-
-    len = 0;
-
-    /* Extract uppercase hexadecimal numbers in reverse order */
-    while (value > 0) {
-        hexDigits[len++] = value % 16;
-        value /= 16;
-    }
-
-    /* Print uppercase hexadecimal digits in correct order */
-    for (i = len - 1; i >= 0; --i)
-    {
-        if (hexDigits[i] < 10)
-        {
-            _putchar(hexDigits[i] + '0');
-        }
-        else
-        {
-            _putchar(hexDigits[i] - 10 + 'A');
-        }
-    }
-
-    return (len);
+	int len, i;
+	unsigned int hexDigits[8];
+	
+	unsigned int value = va_arg(args, unsigned int);
+	
+	/* Handle the case of zero */
+	if (value == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	
+	len = 0;
+	
+	/* Extract uppercase hexadecimal numbers in reverse order */
+	while (value > 0)
+	{
+		hexDigits[len++] = value % 16;
+		value /= 16;
+	}
+	
+	/* Print uppercase hexadecimal digits in correct order */
+	for (i = len - 1; i >= 0; --i)
+	{
+		if (hexDigits[i] < 10)
+		{
+			_putchar(hexDigits[i] + '0');
+		}
+		else
+		{
+			_putchar(hexDigits[i] - 10 + 'A');
+		}
+	}
+	
+	return (len);
 }
