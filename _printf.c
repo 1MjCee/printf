359d77033792b75baa1fc983e89a33048b6a50ce
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-23 11:28:25 +0200

    p2

--- _printf.c
+++ _printf.c
@@ -1,5 +1,3 @@
-#include <stdarg.h>
-#include <string.h>
 #include "main.h"
 int _printf(const char *format, ...)
 {
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef ASD
 #define ASD
 
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 
