a3f0931a6ee138cb55c100af60978cac6108e73a
Jordan Tuyisenge <jordanale.112@gmail.com>
2025-03-23 10:58:36 +0200

    p

--- _printf.c
+++ _printf.c
@@ -1,94 +1,46 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
-
-/**
- * int _printf - prints formatted string on stdout
- * @format: string to print,
- * Return: length of arguments printed.
- */
-
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count;
+	va_list args;
+	int count, i;
+	char sc;
+	char *df;
 
-    va_start(args, format);
-    count = 0;
-
-    if (format != NULL)
-    {
-        process_format_string(format, args, &count);
-    }
-    else
-    {
-        return (0);
-    }
-
-    va_end(args);
-    return (count);
-}
-
-/**
- * int process_format_string - processes the format string for printing,
- * @format: string to print,
- * @args: list of arguments to be printed,
- * @count: Number of arguments to be printed,
- * Return: pointer to number of arguments printed.
- */
-
-int process_format_string(const char *format, va_list args, int *count)
-{
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    *count += print_char(args);
-                    break;
-                case 's':
-                    *count += print_str(args);
-                    break;
-                case '%':
-                    *count += print_percent(args);
-                    break;
-                case 'd':
-                    *count += print_integer_d(args);
-                    break;
-                case 'i':
-                    *count += print_integer_i(args);
-                    break;
-                case 'u':
-                    *count += print_unsigned_integer(args);
-                    break;
-                case 'o':
-                    *count += print_octal_integer(args);
-                    break;
-                case 'x':
-                    *count += print_hexadecimal_integer(args);
-                    break;
-                case 'X':
-                    *count += print_hexadecimal_upper(args);
-                    break;
-                case 'b':
-                    *count += print_binary(args);
-                    break;
-                case 'S':
-                    *count += print_ascii_char(args);
-                    break;
-                default:
-                    /* unsupported specifier, print an error message or handle it in a specific way */
-                    *count += _putchar('%');
-                    *count += _putchar(*format);
-            }
-        } else
-        {
-            *count += _putchar(*format);
-        }
-        format++;
-    }
-    return (*count);
+	va_start(args, format);
+	i = 0;
+	count = 0;
+	while(format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			sc = format[i];
+			switch(sc)
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					df = va_arg(args, char *);
+					printf("%s", df);
+					count = count + strlen(df);
+					break;
+				case '%':
+					printf("%%");
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			printf("%c", format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,26 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef ASD
+#define ASD
 
-#include <unistd.h>
-#include <string.h>
-#include <stdarg.h>
-#include <stddef.h>
-
-int process_format_string(const char *format, va_list args, int *count);
-int print_percent(va_list args);
-int print_char(va_list args);
-int print_str(va_list args);
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_integer_d(va_list args);
-int print_integer_i(va_list args);
-size_t _strlen(const char *str);
-int print_unsigned_integer(va_list args);
-int print_octal_integer(va_list args);
-int print_hexadecimal_integer(va_list args);
-int print_hexadecimal_upper(va_list args);
-int print_binary(va_list args);
-int print_value_hex(int value);
-int print_ascii_char(va_list args);
 
 #endif
